57fd7412321e87933609e37dddd33332894ecb92
Ryan Cramer <ryan@rc-d.net>
2016-10-21 14:14:00 -0400

    BUmp version to 3.0.38

--- wire/core/ProcessWire.php
+++ wire/core/ProcessWire.php
@@ -28,7 +28,7 @@ class ProcessWire extends Wire {
 
 	const versionMajor = 3; 
 	const versionMinor = 0; 
-	const versionRevision = 37; 
+	const versionRevision = 38; 
 	const versionSuffix = '';
 	
 	const indexVersion = 300; // required version for index.php file (represented by PROCESSWIRE define)
