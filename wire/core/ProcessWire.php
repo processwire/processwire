8b96e6b060d304435e822a0f15b2c67927fc1a33
Ryan Cramer <ryan@rc-d.net>
2017-03-10 09:05:08 -0500

    Attempt fix for issue processwire/processwire-issues#206 fix for multi-site config not working

--- wire/core/ProcessWire.php
+++ wire/core/ProcessWire.php
@@ -772,7 +772,6 @@ class ProcessWire extends Wire {
 		$rootPath = rtrim($rootPath, '/');
 		$_rootURL = $rootURL;
 		if(is_null($rootURL)) $rootURL = '/';
-		$sitePath = $rootPath . "/$siteDir/";
 		
 		// check what rootPath is referring to
 		if(strpos($rootPath, "/$siteDir")) {
@@ -834,6 +833,7 @@ class ProcessWire extends Wire {
 		}
 
 		// other default directories
+		$sitePath = $rootPath . "/$siteDir/";
 		$wireDir = "wire";
 		$coreDir = "$wireDir/core";
 		$assetsDir = "$siteDir/assets";
