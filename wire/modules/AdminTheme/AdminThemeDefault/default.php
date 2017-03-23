04c87f195478fb5ff4ac10897246e9ec33a59d8c
Notanotherdotcom <notanotherdotcom@gmail.com>
2017-03-23 22:57:18 +0000

    Branding Upgrade
    Replaces the logo in the Default and Reno admin themes with the new
    version for branding consistency.

--- wire/modules/AdminTheme/AdminThemeDefault/default.php
+++ wire/modules/AdminTheme/AdminThemeDefault/default.php
@@ -31,22 +31,22 @@ if($user->hasPermission('page-edit')) {
 
 $version = $adminTheme->version . 'i';
 
-$config->styles->prepend($config->urls->root . "wire/templates-admin/styles/AdminTheme.css?v=$version"); 
-$config->styles->prepend($config->urls->adminTemplates . "styles/" . ($adminTheme->colors ? "main-$adminTheme->colors" : "main-classic") . ".css?v=$version"); 
-$config->styles->append($config->urls->root . "wire/templates-admin/styles/font-awesome/css/font-awesome.min.css?v=$version"); 
-	
+$config->styles->prepend($config->urls->root . "wire/templates-admin/styles/AdminTheme.css?v=$version");
+$config->styles->prepend($config->urls->adminTemplates . "styles/" . ($adminTheme->colors ? "main-$adminTheme->colors" : "main-classic") . ".css?v=$version");
+$config->styles->append($config->urls->root . "wire/templates-admin/styles/font-awesome/css/font-awesome.min.css?v=$version");
+
 $ext = $config->debug ? "js" : "min.js";
 $config->scripts->append($config->urls->root . "wire/templates-admin/scripts/inputfields.$ext?v=$version");
-$config->scripts->append($config->urls->root . "wire/templates-admin/scripts/main.$ext?v=$version"); 
+$config->scripts->append($config->urls->root . "wire/templates-admin/scripts/main.$ext?v=$version");
 $config->scripts->append($config->urls->adminTemplates . "scripts/main.$ext?v=$version");
-	
+
 require_once(dirname(__FILE__) . "/AdminThemeDefaultHelpers.php");
 /** @var AdminThemeDefaultHelpers $helpers */
 $helpers = $this->wire(new AdminThemeDefaultHelpers());
 $extras = $adminTheme->getExtraMarkup();
 
 ?><!DOCTYPE html>
-<html class="pw" lang="<?php echo $helpers->_('en'); 
+<html class="pw" lang="<?php echo $helpers->_('en');
 	/* this intentionally on a separate line */ ?>">
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
@@ -62,22 +62,22 @@ $extras = $adminTheme->getExtraMarkup();
 	<?php foreach($config->styles as $file) echo "\n\t<link type='text/css' href='$file' rel='stylesheet' />"; ?>
 
 	<?php foreach($config->scripts as $file) echo "\n\t<script type='text/javascript' src='$file'></script>"; ?>
-	
+
 	<?php echo $extras['head']; ?>
 
 </head>
 <body class='<?php echo $helpers->renderBodyClass(); ?>'>
 
-	<?php 
-	echo $helpers->renderAdminNotices($notices); 
-	echo $extras['notices']; 
+	<?php
+	echo $helpers->renderAdminNotices($notices);
+	echo $extras['notices'];
 	?>
 
 	<div id="masthead" class="pw-masthead ui-helper-clearfix">
 		<div class="pw-container container">
 
 			<a id='logo' href='<?php echo $config->urls->admin?>'>
-				<img width='130' src="<?php echo $config->urls->adminTemplates?>styles/images/logo.png" alt="ProcessWire" />
+                <img width='144' src="<?php echo $config->urls->adminTemplates?>styles/images/logo.png" alt="ProcessWire" />
 			</a>
 
 			<?php 
