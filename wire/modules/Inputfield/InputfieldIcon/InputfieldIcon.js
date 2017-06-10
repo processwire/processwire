a06865d861b13a56f9a5ef696fbbac5cff6634d0
Roland Toth <contact@rolandtoth.hu>
2017-06-10 23:49:30 +0200

    Update InputfieldIcon.js

--- wire/modules/Inputfield/InputfieldIcon/InputfieldIcon.js
+++ wire/modules/Inputfield/InputfieldIcon/InputfieldIcon.js
@@ -1,81 +1,74 @@
-
 $(document).ready(function() {
-	
+
 	$(".InputfieldIcon select").change(function() {
-		
-		var $select = $(this); 
+
+		var $select = $(this);
 		var val = $select.val();
-	
-		if(val.length > 0) {
+
+		if (val.length > 0) {
 			$select.closest(".InputfieldIcon").find(".InputfieldHeader > i.fa:first-child")
-				.attr('class', 'fa ' + val) 
+				.attr('class', 'fa ' + val)
 				.parent().effect('highlight', 500);
 			var $all = $select.siblings(".InputfieldIconAll");
-			if($all.is(":visible")) {
-				$all.find('.on').removeClass('on').mouseout();
-				var $icon = $all.find("." + val).parent('span');
-				if(!$icon.hasClass('on')) $icon.addClass('on').mouseover();
+			if ($all.is(":visible")) {
+				$all.find('.on').removeClass('on');
+				$all.find("." + val).addClass('on');
 			}
 		}
-		
-		$select.removeClass('on'); 
+
+		$select.removeClass('on');
 	});
-	
+
 	$(".InputfieldIconAll").hide();
-	
-	$("a.InputfieldIconShowAll").click(function() {
-	
-		var $link = $(this);
-		var $all = $link.siblings(".InputfieldIconAll");
-		var $select = $link.siblings("select"); 
-		
-		if($all.is(":visible")) {
-			$all.slideUp('fast', function() {
-				$all.html(''); 
-			}); 
+
+
+	$("a.InputfieldIconShowAll").on('click', function() {
+
+		var $all = $(this).siblings(".InputfieldIconAll");
+		var $select = $(this).siblings("select");
+
+		if ($all.is(":visible")) {
+			$all.slideUp('fast');
+			//$all.off('click', 'i');
 			return false;
 		}
-		
-		$select.children("option").each(function() {
-			var val = $(this).attr('value'); 
-			if(val.length == 0) return;
-			var $icon = $("<i class='fa fa-fw'></i>")
-				.addClass(val)
-				.attr('data-name', val)
-				.css('cursor', 'pointer')
-				.attr('title', val); 
-			var $span = $("<span />")
-				.css('padding', '2px 2px 2px 2px')
-				.css('margin-right', '10px')
-				.css('line-height', '30px')
-				.append($icon);
-			$all.append($span); 
-		}); 
-		
+
 		$all.slideDown('fast', function() {
+
+			if(!$all.hasClass('initialized')) {
+				
+				$all.addClass('initialized');
+				
+				$select.children("option").each(function() {
+					var val = $(this).attr('value');
+					if (val.length == 0) return;
+					$all.append("<i class='fa fw " + val + "' title='" + val + "'>");
+				});
 			
-			$all.on('click', 'span', function() {
-				$all.find('.on').removeClass('on').mouseout();
-				$(this).addClass('on').mouseover();
-				if(!$select.hasClass('on')) $select.val($(this).find('i.fa').attr('data-name')).change();
 
-			});
-			$all.on('mouseover', 'span', function() {
-				$(this).css('background-color', 'red').css('color','white'); 
-			});
-			$all.on('mouseout', 'span', function() {
-				if(!$(this).hasClass('on')) {
-					$(this).css('background-color', 'inherit').css('color', 'inherit'); 
-				}
-			});
+				$all.on('click', 'i', function() {
 
-			var val = $select.val();	
-			if(val.length > 0) $all.find("." + val).each(function() {
-				$(this).parent('span').addClass('on').mouseover();
-			}); 
-		}); 
-	
+					if ($(this).hasClass('on')) {
+						$(this).removeClass('on');
+						$select.val('').change();
+						return;
+					}
+
+					$all.find('.on').removeClass('on');
+					$(this).addClass('on');
+
+					if (!$select.hasClass('on')) {
+						$select.val($(this).attr('title')).change();
+					}
+
+				});
+
+				var val = $select.val();
+				if (val.length > 0) $all.find("." + val).addClass('on');
+			}
+		});
+
 		return false;
-	}); 
-	
+	});
+
 });
