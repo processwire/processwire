b32a2f00584c1ded1ab0bee74a0ea115f4a648ba
Ryan Cramer <ryan@rc-d.net>
2017-06-16 14:34:53 -0400

    Bump version to 3.0.64

--- wire/core/ProcessWire.php
+++ wire/core/ProcessWire.php
@@ -45,7 +45,7 @@ class ProcessWire extends Wire {
 	 * Reversion revision number
 	 * 
 	 */
-	const versionRevision = 63;
+	const versionRevision = 64;
 
 	/**
 	 * Version suffix string (when applicable)
