7873422f9eaaa0477ec8db282a2456ae9b51cb7c
Ryan Cramer <ryan@rc-d.net>
2017-08-11 11:06:31 -0400

    Add ProcessPagesExportImport module to core, and bump version to 3.0.71. To install this new module, go to Modules > Refresh, then Modules > Core > Process > Pages Export/Import > Install

--- wire/core/ProcessWire.php
+++ wire/core/ProcessWire.php
@@ -45,7 +45,7 @@ class ProcessWire extends Wire {
 	 * Reversion revision number
 	 * 
 	 */
-	const versionRevision = 70;
+	const versionRevision = 71;
 
 	/**
 	 * Version suffix string (when applicable)
