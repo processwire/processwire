828a80f1f697aa5ba99fbe7df0991599a314c588
Ryan Cramer <ryan@rc-d.net>
2018-06-15 06:38:04 -0400

    Fix issue processwire/processwire-issues#621 where PageFrontEdit module config modal in AdminThemeDefault was showing extra/unnecessary buttons on modal dialog

--- wire/modules/Page/PageFrontEdit/PageFrontEditConfig.php
+++ wire/modules/Page/PageFrontEdit/PageFrontEditConfig.php
@@ -126,7 +126,7 @@ class PageFrontEditConfig extends ModuleConfig {
 			$f->description .= " [$moreLabel](https://processwire.com/api/modules/front-end-editing/#option-a-automatic-editing)";
 			$href = "{$this->config->urls->admin}module/edit?name=PageFrontEdit&modal=1&";
 			$f->value =
-				"<p><a class='pw-modal ui-button ui-state-default' data-buttons='.ui-button' data-autoclose href='$href'>" .
+				"<p><a class='pw-modal ui-button ui-state-default' data-buttons='#Inputfield_submit_save_module' data-autoclose href='$href'>" .
 				"<i class='fa fa-gear fa-fw'></i>" . $this->_('Configure') . "</a></p>";
 			if(in_array($field->id, $module->inlineEditFields)) {
 				$f->notes = $this->_('This option is currently ENABLED for this field.'); 
