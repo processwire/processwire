c41b670330ddea9839ac1e77711fccfd3016a7e4
Ryan Cramer <ryan@processwire.com>
2019-03-04 10:54:07 -0500

    Fix issue processwire/processwire-issues#305

--- wire/modules/AdminTheme/AdminThemeDefault/styles/sass/compile.sh
+++ wire/modules/AdminTheme/AdminThemeDefault/styles/sass/compile.sh
@@ -1,4 +1,4 @@
-sass --style=compressed --sourcemap=none --no-cache main-warm.scss:../main-warm.css
-sass --style=compressed --sourcemap=none --no-cache main-classic.scss:../main-classic.css
-sass --style=compressed --sourcemap=none --no-cache main-futura.scss:../main-futura.css
-sass --style=compressed --sourcemap=none --no-cache main-modern.scss:../main-modern.css
+sass --style=compressed --no-source-map main-warm.scss:../main-warm.css
+sass --style=compressed --no-source-map main-classic.scss:../main-classic.css
+sass --style=compressed --no-source-map main-futura.scss:../main-futura.css
+sass --style=compressed --no-source-map main-modern.scss:../main-modern.css
--- wire/modules/AdminTheme/AdminThemeReno/styles/compile.sh
+++ wire/modules/AdminTheme/AdminThemeReno/styles/compile.sh
@@ -1,4 +1,4 @@
-sass --style=compressed main.scss:main.css
-sass --style=compressed classic.scss:classic.css
-sass --style=compressed blue.scss:blue.css
-sass --style=compressed blue.scss:blue.css
+sass --no-source-map --style=compressed main.scss:main.css
+sass --no-source-map --style=compressed classic.scss:classic.css
+sass --no-source-map --style=compressed blue.scss:blue.css
+sass --no-source-map --style=compressed blue.scss:blue.css
