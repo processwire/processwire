0f6b59a9bed191b5c387fc4f591561ba38b824b4
Chris Thomson <chris.thomson@nbcommunication.com>
2019-03-27 10:49:55 +0000

    Update Pageimage.php
    Correction of option name

--- wire/core/Pageimage.php
+++ wire/core/Pageimage.php
@@ -1555,7 +1555,7 @@ class Pageimage extends Pagefile {
 	 * @param array $options See options for getVariations() method to limit what variations are removed, plus these:
 	 *  - `dryRun` (bool): Do not remove now and instead only return the filenames of variations that would be deleted (default=false).
 	 *  - `getFiles` (bool): Return deleted filenames? Also assumed if the test option is used (default=false). 
-	 * @return $this|array Returns $this by default, or array of deleted filenames if the `returnFiles` option is specified
+	 * @return $this|array Returns $this by default, or array of deleted filenames if the `getFiles` option is specified
 	 *
 	 */
 	public function removeVariations(array $options = array()) {
