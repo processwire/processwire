5235ab2a8936ceb5aa9214b73db126a4743903be
Ryan Cramer <ryan@processwire.com>
2019-06-07 12:11:52 -0400

    Add $session->getVal() methods which work the same as get() but let you speicfy the fallback value

--- wire/core/Session.php
+++ wire/core/Session.php
@@ -462,6 +462,24 @@ class Session extends Wire implements \IteratorAggregate {
 	}
 
 	/**
+	 * Get a session variable or return $val argument if session value not present
+	 * 
+	 * This is the same as get() except that it lets you specify a fallback return value in the method call. 
+	 * For a namespace version use `Session::getValFor()` instead. 
+	 * 
+	 * @param string $key Name of session variable to retrieve.
+	 * @param mixed $val Fallback value to return if session does not have it.
+	 * @return mixed Returns value of seession variable, or NULL if not found. 
+	 * @since 3.0.133
+	 * 
+	 */
+	public function getVal($key, $val = null) {
+		$value = $this->get($key);
+		if($value === null) $value = $val;
+		return $value;
+	}
+
+	/**
 	 * Get all session variables in an associative array
  	 *
 	 * @param object|string $ns Optional namespace
@@ -537,6 +555,25 @@ class Session extends Wire implements \IteratorAggregate {
 		if(!is_array($data)) $data = array();
 		if($key === '') return $data;
 		return isset($data[$key]) ? $data[$key] : null;
+	}
+	
+	/**
+	 * Get a session variable or return $val argument if session value not present
+	 *
+	 * This is the same as get() except that it lets you specify a fallback return value in the method call.
+	 * For a namespace version use `Session::getValFor()` instead.
+	 *
+	 * @param string|object $ns Namespace string or object
+	 * @param string $key Specify variable name to retrieve
+	 * @param mixed $val Fallback value if session does not have one
+	 * @return mixed
+	 * @since 3.0.133
+	 *
+	 */
+	public function getValFor($ns, $key, $val = null) {
+		$value = $this->getFor($ns, $key);
+		if($value === null) $value = $val;
+		return $value;
 	}
 
 	/**
