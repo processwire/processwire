2adf4b0707c0eaa1a6381eb1109c64e36566ad3c
Ryan Cramer <ryan@processwire.com>
2020-01-29 05:58:59 -0500

    Updates to $config->fileContentTypes

--- wire/config.php
+++ wire/config.php
@@ -596,18 +596,24 @@ $config->contentTypes = array(
  */
 $config->fileContentTypes = array(
 	'?' => '+application/octet-stream',
+	'txt' => '+text/plain',
+	'csv' => '+text/csv',
 	'pdf' => '+application/pdf',
 	'doc' => '+application/msword',
-	'docx' => '+application/msword',
-	'xls' => '+application/excel',
-	'xlsx' => '+application/excel',
+	'docx' => '+application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+	'xls' => '+application/vnd.ms-excel',
+	'xlsx' => '+application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+	'ppt' => '+application/vnd.ms-powerpoint',
+	'pptx' => '+application/vnd.openxmlformats-officedocument.presentationml.presentation',
 	'rtf' => '+application/rtf',
 	'gif' => 'image/gif',
 	'jpg' => 'image/jpeg',
 	'jpeg' => 'image/jpeg',
 	'png' => 'image/png',
 	'svg' => 'image/svg+xml',
-	'webp' => 'image/webp'
+	'webp' => 'image/webp',
+	'zip' => '+application/zip',
+	'mp3' => 'audio/mpeg',
 	);
 
 
