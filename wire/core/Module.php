6bd790101126ae20e57eec7bcd3a753a6a616210
Ryan Cramer <ryan@processwire.com>
2020-04-15 16:33:57 -0400

    Fix issue processwire/processwire-issues#1122

--- wire/core/Module.php
+++ wire/core/Module.php
@@ -332,7 +332,7 @@
  * to support the module from one version to another. The previous known version ($fromVersion) and new
  * version ($toVersion) are provided as arguments.
  * 
- * If implemented, uninstall() methods typically are defined hookable as `public function ___upgrade(...)`. 
+ * If implemented, upgrade() methods typically are defined hookable as `public function ___upgrade(...)`. 
  * If the upgrade cannot proceed for some reason, this method should throw a `WireException`. 
  * 
  * 
