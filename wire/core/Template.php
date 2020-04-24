e36667624fb22ced0acee3b9da7c5d5027958b74
Ryan Cramer <ryan@processwire.com>
2020-04-24 12:37:10 -0400

    Fix typo in phpdoc of Template.php

--- wire/core/Template.php
+++ wire/core/Template.php
@@ -69,8 +69,8 @@
  * @property int|string $urlSegments Allow URL segments on pages? (0=no, 1=yes (all), string=space separted list of segments to allow) #pw-group-URLs
  * @property int $https Use https? (0 = http or https, 1 = https only, -1 = http only) #pw-group-URLs
  * @property int $slashUrls Page URLs should have a trailing slash? 1 = yes, 0 = no	 #pw-group-URLs
- * @property string|int $slashPageNum Should PageNum segments have a trailing slash? (blank=either, 1=yes, 0=no) applies only if allowPageNum!=0. #pw-group-URLs
- * @property string|int $slashUrlSegments Should last URL segment have a trailing slash? (blank=either, 1=yes, 0=no) applies only if urlSegments!=0. #pw-group-URLs
+ * @property string|int $slashPageNum Should PageNum segments have a trailing slash? (0=either, 1=yes, -1=no) applies only if allowPageNum!=0. #pw-group-URLs
+ * @property string|int $slashUrlSegments Should last URL segment have a trailing slash? (0=either, 1=yes, -1=no) applies only if urlSegments!=0. #pw-group-URLs
  * 
  * Files
  * 
