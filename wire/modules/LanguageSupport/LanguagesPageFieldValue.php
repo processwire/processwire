d3b1ab2983108a0410f7c66997f0478e7f3e02e6
Ryan Cramer <ryan@processwire.com>
2020-12-11 12:05:09 -0500

    Add LanguagesPageFieldValue::setToInputfield to accompany existing setFromInputfield method

--- wire/modules/LanguageSupport/LanguagesPageFieldValue.php
+++ wire/modules/LanguageSupport/LanguagesPageFieldValue.php
@@ -152,7 +152,7 @@ class LanguagesPageFieldValue extends Wire implements LanguagesValueInterface, \
 	}
 
 	/**
-	 * Given an Inputfield with multi language values, this grabs and populates the language values from it
+	 * Grab language values from Inputfield and populate to this object
 	 *
 	 * @param Inputfield $inputfield
 	 *
@@ -166,6 +166,20 @@ class LanguagesPageFieldValue extends Wire implements LanguagesValueInterface, \
 				$key = 'value' . $language->id; 
 			}
 			$this->setLanguageValue($language->id, $inputfield->$key); 
+		}
+	}
+
+	/**
+	 * Populate language values from this object to given Inputfield
+	 *
+	 * @param Inputfield $inputfield
+	 * @since 3.0.170
+	 *
+	 */
+	public function setToInputfield(Inputfield $inputfield) {
+		foreach($this->wire()->languages as $language) {
+			$key = $language->isDefault ? "value" : "value$language->id";
+			$inputfield->set($key, $this->getLanguageValue($language->id));
 		}
 	}
 
