54c49f37f84c604bcbf664a8cd2c88bc5644fa4a
Ryan Cramer <ryan@processwire.com>
2021-04-22 10:33:33 -0400

    Add new interfaces for Inputfield modules

--- wire/core/Interfaces.php
+++ wire/core/Interfaces.php
@@ -556,9 +556,67 @@ interface WireProfilerInterface {
 interface InputfieldHasArrayValue { }
 
 /**
+ * Inputfield that doesn’t have an array value by default but can return array value or accept it
+ * 
+ * @since 3.0.176
+ *
+ */
+interface InputfieldSupportsArrayValue {
+	/**
+	 * @return array
+	 * 
+	 */
+	public function getArrayValue();
+
+	/**
+	 * @param array $value
+	 * 
+	 */
+	public function setArrayValue(array $value);
+}
+
+/**
+ * Inputfield that has a text value by default
+ * 
+ * @since 3.0.176
+ * 
+ */
+interface InputfieldHasTextValue { }
+
+/**
  * Inputfield that has a sortable value (usually in addition to InputfieldHasArrayValue)
  *
  */
 interface InputfieldHasSortableValue { }
+
+/**
+ * Inputfield that supports selectable options
+ * 
+ * @since 3.0.176
+ *
+ */
+interface InputfieldHasSelectableOptions {
+	/**
+	 * Add a selectable option
+	 * 
+	 * @param string|int $value
+	 * @param string|null $label
+	 * @param array|null $attributes
+	 * @return self|$this
+	 * 
+	 */
+	public function addOption($value, $label = null, array $attributes = null);
+	
+	/**
+	 * Add selectable option with label, optionally for specific language
+	 *
+	 * @param string|int $value
+	 * @param string $label
+	 * @param Language|null $language
+	 * @return self|$this
+	 *
+	 */
+	public function addOptionLabel($value, $label, $language = null);
+}
 
 
