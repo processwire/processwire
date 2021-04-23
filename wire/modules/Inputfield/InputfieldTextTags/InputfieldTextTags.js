253e6ba6610cdc36e6abc62a5d7573dfaad7d55f
Ryan Cramer <ryan@processwire.com>
2021-04-23 15:07:31 -0400

    Additional updates to ProcessTemplate and InputfieldTextTags

--- wire/modules/Inputfield/InputfieldTextTags/InputfieldTextTags.js
+++ wire/modules/Inputfield/InputfieldTextTags/InputfieldTextTags.js
@@ -5,79 +5,72 @@ function InputfieldTextTags($parent) {
 	var $inputs = jQuery('.InputfieldTextTagsInput:not(.selectized)', $parent);
 	var $selects = jQuery('.InputfieldTextTagsSelect:not(.selectized)', $parent);
 	
-	if($inputs.length) {
-		$inputs.selectize({
-			plugins: ['remove_button', 'drag_drop'],
-			delimiter: ' ',
-			persist: false,
-			createOnBlur: true,
-			submitOnReturn: false,
-			create: function(input) {
-				return {
-					value: input,
-					text: input
-				}
+	var defaults = {
+		plugins: [ 'remove_button', 'drag_drop' ],
+		delimiter: ' ',
+		persist: true, // If false, items created by the user will not show up as available options once they are unselected.
+		submitOnReturn: false,
+		openOnFocus: true, // Show the dropdown immediately when the control receives focus.
+		closeAfterSelect: true, // If true, the dropdown will be closed after a selection is made.
+		copyClassesToDropdown: false,
+		createOnBlur: true, // If true, when user exits the field (clicks outside of input), a new option is created and selected (if create setting is enabled).
+		selectOnTab: true, // If true, the tab key will choose the currently selected item.
+		maxItems: null, // The max number of items the user can select. 1 makes the control mono-selection, null allows an unlimited number of items.
+		create: function(input) {
+			return {
+				value: input,
+				text: input
 			}
+		}
+	};
+	
+	if($inputs.length) {
+		$inputs.each(function() {
+			$input = $(this);
+			var o = JSON.parse($input.attr('data-opts'));
+			var options = defaults;
+			options.delimiter = o.delimiter;
+			options.closeAfterSelect = o.closeAfterSelect;
+			options.persist = false;
+			$input.selectize(options);
 		});
 	}
 
 	if($selects.length) {
 		$selects.each(function() {
 			var $select = $(this);
-			var configName = $select.attr('data-cfgname');
-			var allowUserTags = $select.hasClass('InputfieldTextTagsSelectOnly') ? false : true;
-			var tags = [];
+			var o = JSON.parse($select.attr('data-opts'));
+			var cfgName = typeof o.cfgName === "undefined" ? '' : o.cfgName;
+			var tags = cfgName.length ? ProcessWire.config[cfgName] : o.tags;
 			var tagsList = [];
 			var n = 0;
-			if(configName.length) {
-				tags = ProcessWire.config[configName];
-			} else {
-				tags = $select.attr('data-tags'); 
-				tags = JSON.parse(tags);
-			}
+			
 			for(var tag in tags) {
 				var label = tags[tag];
 				tagsList[n] = { value: tag, label: label };
 				n++;
 			}
-			$select.selectize({
-				plugins: ['remove_button', 'drag_drop'],
-				delimiter: ' ',
+			
+			var options = jQuery.extend(defaults, {
+				allowUserTags: o.allowUserTags,
+				delimiter: o.delimiter,
+				closeAfterSelect: o.closeAfterSelect,
 				persist: true,
-				submitOnReturn: false,
-				closeAfterSelect: true,
-				copyClassesToDropdown: false, 
-				createOnBlur: true,
-				maxItems: null,
 				valueField: 'value',
 				labelField: 'label',
-				searchField: ['value', 'label'],
+				searchField: [ 'value', 'label' ],
 				options: tagsList,
-				create: function(input) {
-					return {
-						value: input,
-						text: input
-					}
-				},
 				createFilter: function(input) {
-					if(allowUserTags) return true;
+					if(o.allowUserTags) return true;
 					allow = false;
 					for(var n = 0; n < tags.length; n++) {
-						if(typeof tagsList[input] !== "undefined") {
+						if(typeof tags[input] !== "undefined") {
 							allow = true;
 							break;
 						}
 					}
 					return allow;
 				},
-				/*
-				onDropdownOpen: function($dropdown) {
-					$dropdown.closest('li, .InputfieldImageEdit').css('z-index', 100);
-				},
-				onDropdownClose: function($dropdown) {
-					$dropdown.closest('li, .InputfieldImageEdit').css('z-index', 'auto');
-				},
-				*/
 				render: {
 					item: function(item, escape) {
 						if(typeof item.label === "undefined" || !item.label.length) item.label = item.value;
@@ -88,7 +81,17 @@ function InputfieldTextTags($parent) {
 						return '<div>' + escape(item.label) + '</div>';
 					}
 				}
-			});
+				/*
+				onDropdownOpen: function($dropdown) {
+					$dropdown.closest('li, .InputfieldImageEdit').css('z-index', 100);
+				},
+				onDropdownClose: function($dropdown) {
+					$dropdown.closest('li, .InputfieldImageEdit').css('z-index', 'auto');
+				},
+				*/
+			}); 
+				
+			$select.selectize(options);
 		}); 
 	}
 }
