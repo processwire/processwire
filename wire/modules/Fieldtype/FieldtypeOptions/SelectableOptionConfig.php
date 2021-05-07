451455e035f779e8c2bcfd402eca329d68c2de31
jmartsch <jmartsch@gmail.com>
2021-05-07 14:30:02 -0400

    Add PR #133 - use better non-technical message for SelectableOptionConfig

--- wire/modules/Fieldtype/FieldtypeOptions/SelectableOptionConfig.php
+++ wire/modules/Fieldtype/FieldtypeOptions/SelectableOptionConfig.php
@@ -217,9 +217,9 @@ class SelectableOptionConfig extends Wire {
 			if($f instanceof InputfieldHasArrayValue && !is_array($initValue) && !empty($initValue)) $initValue = explode(' ', $initValue);
 			$f->attr('value', $initValue);
 			if(!$field->required && !$field->requiredIf) {
-				$f->notes = $this->_('Please note: your selections here do not become active unless a value is *always* required for this field. See the "required" option on the Input tab of your field settings.');
+				$f->notes = $this->_('Please note: Your pre-selection is not active, as this field is not a required field. Activate the option “required” in the input tab of the field.');
 			} else {
-				$f->notes = $this->_('This feature is active since a value is always required.'); 
+				$f->notes = $this->_('The pre-selection is active because this field is a required field.');
 			}
 			$inputfields->add($f); 
 			$inputfields->add($this->getInstructions());
