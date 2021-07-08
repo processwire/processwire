498010ad8f795b4bafb3547dd27a8e2ba31e254f
Ryan Cramer <ryan@processwire.com>
2021-07-08 11:36:19 -0400

    Add a new FieldtypeHasFiles interface and update FieldtypeFile and FieldtypeImage to implement it. This interface is intended to be implemented by any Fieldtype that works with files to provide a common interface among them.

--- wire/core/Interfaces.php
+++ wire/core/Interfaces.php
@@ -166,6 +166,42 @@ interface WireTrackable {
  */
 interface FieldtypePageTitleCompatible { }
 
+/**
+ * Indicates Fieldtype manages files
+ *
+ */
+interface FieldtypeHasFiles {
+	/**
+	 * Whether or not given Page/Field has any files connected with it
+	 * 
+	 * @param Page $page
+	 * @param Field $field
+	 * @return bool
+	 * 
+	 */
+	public function hasFiles(Page $page, Field $field);
+	
+	/**
+	 * Get array of full path/file for all files managed by given page and field
+	 * 
+	 * @param Page $page
+	 * @param Field $field
+	 * @return array
+	 * 
+	 */
+	public function getFiles(Page $page, Field $field);
+
+	/**
+	 * Get path where files are (or would be) stored
+	 * 
+	 * @param Page $page
+	 * @param Field $field
+	 * @return string
+	 * 
+	 */
+	public function getFilesPath(Page $page, Field $field); 
+}
+
 
 /**
  * Indicates that an Inputfield provides tree selection capabilities
