062f2293d0e1b920929574cf05bad50d3c1591b2
Ryan Cramer <ryan@processwire.com>
2022-01-07 14:18:32 -0500

    Bump version to 3.0.192

--- wire/core/ProcessWire.php
+++ wire/core/ProcessWire.php
@@ -17,7 +17,7 @@ require_once(__DIR__ . '/boot.php');
  * ~~~~~
  * #pw-body
  * 
- * ProcessWire 3.x, Copyright 2021 by Ryan Cramer
+ * ProcessWire 3.x, Copyright 2022 by Ryan Cramer
  * https://processwire.com
  *
  * Default API vars (A-Z) 
@@ -79,7 +79,7 @@ class ProcessWire extends Wire {
 	 * Reversion revision number
 	 * 
 	 */
-	const versionRevision = 191;
+	const versionRevision = 192;
 
 	/**
 	 * Version suffix string (when applicable)
