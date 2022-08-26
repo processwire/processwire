cbb5133d46bb4cf990543e27980733f219c89d28
Ryan Cramer <ryan@processwire.com>
2022-08-26 14:01:35 -0400

    Bump version to 3.0.204

--- wire/core/ProcessWire.php
+++ wire/core/ProcessWire.php
@@ -79,7 +79,7 @@ class ProcessWire extends Wire {
 	 * Reversion revision number
 	 * 
 	 */
-	const versionRevision = 203;
+	const versionRevision = 204;
 
 	/**
 	 * Version suffix string (when applicable)
