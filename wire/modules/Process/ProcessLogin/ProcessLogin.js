ec8555545e7c9a62eb9b5ad44e35e22100a5f171
Ryan Cramer <ryan@processwire.com>
2022-10-06 13:31:51 -0400

    Update ProcessLogin to auto-refresh the login form every 5 minutes of non-activity. This helps to avoid cases where a login form sits for a long period and has its CSRF key expire. It can prevent this error at login: "This request was aborted because it appears to be forged."

--- wire/modules/Process/ProcessLogin/ProcessLogin.js
+++ wire/modules/Process/ProcessLogin/ProcessLogin.js
@@ -13,12 +13,35 @@ $(document).ready(function() {
 	}
 	$("#login_hidpi").val(hidpi ? 1 : 0);
 
+	var startTime = parseInt($('#login_start').val()); // GMT/UTC
+	var maxSeconds = 300; // max age for login form before refreshing it (300=5min)
+	
+	// force refresh of login form if 5 minutes go by without activity
+	var watchTime = function() {
+		var ts = Math.floor(new Date().getTime() / 1000);
+		var elapsedSeconds = ts - startTime;
+		if(elapsedSeconds > maxSeconds) {
+			window.location.href = './?r=' + ts;
+		}
+	};
+	
+	// reload immediately if we received browser cached login form watchTime(); 
+	watchTime();
+	
+	var interval = setInterval(watchTime, 5000);
+	
+	$('#login_name, #login_pass').on('keydown', function() {
+		clearInterval(interval);
+		interval = setInterval(watchTime, 5000);
+	});
+	
 	// via @Toutouwai #84
 	$('#ProcessLoginForm').submit(function() {
 		var $html = $('html');
 		var touch = $html.data('whatintent') == 'touch' || $html.data('whatinput') == 'touch';
+		clearInterval(interval);
 		$('#login_touch').val(touch ? 1 : 0);
 		$('#login_width').val($(window).width());
 	});
-	
+
 }); 
