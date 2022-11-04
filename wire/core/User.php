d9bfba5c74ed10c31e8512f31b4551b3b67e12f3
Ryan Cramer <ryan@processwire.com>
2022-11-04 15:34:27 -0400

    Fix issue processwire/processwire-issues#1630

--- wire/core/User.php
+++ wire/core/User.php
@@ -523,15 +523,14 @@ class User extends Page {
 	}
 
 	/**
-	 * Get the value for a non-native User field
+	 * Get value
 	 * 
 	 * @param string $key
-	 * @param string|Selectors|array $selector
 	 * @return null|mixed
 	 *
 	 */
-	protected function getFieldValue($key, $selector = '') {
-		$value = parent::getFieldValue($key, $selector);
+	public function get($key) {
+		$value = parent::get($key);
 		if(!$value && $key === 'language') {
 			$languages = $this->wire()->languages;
 			if($languages) $value = $languages->getDefault();
