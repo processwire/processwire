4960d8f891b7f84687e8f485a57d13d59fd194df
Ryan Cramer <ryan@processwire.com>
2023-03-17 09:39:08 -0400

    Improve documentation for $session->close(); method, explaining in detail when you might use the method.

--- wire/core/Session.php
+++ wire/core/Session.php
@@ -1428,7 +1428,11 @@ class Session extends Wire implements \IteratorAggregate {
 	/**
 	 * Manually close the session, before program execution is done
 	 * 
-	 * #pw-internal
+	 * A user session is limited to rendering one page at a time, unless the session is closed
+	 * early. Use this when you have a request that may take awhile to render (like a request 
+	 * rendering a sitemap, etc.) and you don't need to get/save session data. By closing the session 
+	 * before starting a render, you can release the session to be available for the user to view
+	 * other pages while the slower page render continues. 
 	 * 
 	 */
 	public function close() {
