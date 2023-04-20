440f649e3979d39cda2ae188cd704ddcffbe7d29
Ryan Cramer <ryan@processwire.com>
2023-04-20 11:01:51 -0400

    Minor updates to site-blank profile, primarily to replace default rich-text editor with TinyMCE for new site-blank installs

--- site-blank/config.php
+++ site-blank/config.php
@@ -17,7 +17,7 @@
  * This file is licensed under the MIT license
  * https://processwire.com/about/license/mit/
  *
- * ProcessWire 3.x, Copyright 2022 by Ryan Cramer
+ * ProcessWire 3.x, Copyright 2023 by Ryan Cramer
  * https://processwire.com
  *
  */
@@ -47,4 +47,3 @@ $config->appendTemplateFile = '_main.php';
 $config->templateCompile = false;
 
 /*** INSTALLER CONFIG ********************************************************************/
-
