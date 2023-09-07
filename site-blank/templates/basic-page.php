7ed74281adb596cde5c09dbddfe4831a5f03a608
Ryan Cramer <ryan@processwire.com>
2023-09-07 08:35:01 -0400

    Add suggestion from processwire/processwire-issues#1806

--- site-blank/templates/basic-page.php
+++ site-blank/templates/basic-page.php
@@ -1,11 +1,15 @@
 <?php namespace ProcessWire; 
 
 // Template file for pages using the “basic-page” template
+// -------------------------------------------------------
+// The #content div in this file will replace the #content div in _main.php
+// when the Markup Regions feature is enabled, as it is by default. 
+// You can also append to (or prepend to) the #content div, and much more. 
+// See the Markup Regions documentation:
+// https://processwire.com/docs/front-end/output/markup-regions/
 
 ?>
-
 
 <div id="content">
 	Basic page content 
 </div>	
-
--- site-blank/templates/home.php
+++ site-blank/templates/home.php
@@ -1,10 +1,15 @@
 <?php namespace ProcessWire;
 
 // Template file for “home” template used by the homepage
+// ------------------------------------------------------
+// The #content div in this file will replace the #content div in _main.php
+// when the Markup Regions feature is enabled, as it is by default. 
+// You can also append to (or prepend to) the #content div, and much more. 
+// See the Markup Regions documentation:
+// https://processwire.com/docs/front-end/output/markup-regions/
 
 ?>
 
 <div id="content">
 	Homepage content 
 </div>	
-	
