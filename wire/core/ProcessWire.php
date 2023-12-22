86c0af08d0f8f76dd46852eb125bf20daa010f8e
Ryan Cramer <ryan@processwire.com>
2023-12-22 12:50:08 -0500

    Bump version to 3.0.233

--- wire/core/ProcessWire.php
+++ wire/core/ProcessWire.php
@@ -54,6 +54,7 @@ require_once(__DIR__ . '/boot.php');
  * @property Users $users
  * @property ProcessWire $wire
  * @property WireShutdown $shutdown
+ * @property PagesVersions|null $pagesVersions
  * 
  * @method init()
  * @method ready()
@@ -79,7 +80,7 @@ class ProcessWire extends Wire {
 	 * Reversion revision number
 	 * 
 	 */
-	const versionRevision = 232;
+	const versionRevision = 233;
 
 	/**
 	 * Version suffix string (when applicable)
