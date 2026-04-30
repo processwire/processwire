79dfe6f329d96ec943412578fce571b6c3c50d76
Ryan Cramer <ryan@processwire.com>
2026-04-30 12:24:06 -0400

    Update ProcessWire installer to have the core SQL self contained within it, so that it gets deleted when the installer is deleted (rather than hanging out forever in wire/core/install.sql)

--- wire/core/WireException.php
+++ wire/core/WireException.php
@@ -262,26 +262,3 @@ class WireCSRFException extends WireException {}
  */
 class WireFilesException extends WireException {}
 
-/**
- * Thrown when a requested Process or Process method is requested that doesn’t exist
- *
- */
-class ProcessController404Exception extends Wire404Exception { }
-
-/**
- * Thrown when the user doesn’t have access to execute the requested Process or method
- *
- */
-class ProcessControllerPermissionException extends WirePermissionException { }
-
-/**
- * Thrown by PageFinder when an error occurs trying to find pages
- * 
- */
-class PageFinderException extends WireException { }
-
-/**
- * Thrown by PageFinder when it detects an error in the syntax of a given page-finding selector
- *
- */
-class PageFinderSyntaxException extends PageFinderException { }
