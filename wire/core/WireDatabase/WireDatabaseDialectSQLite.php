de969af28170f33fdfb9729d072f62b8a34a435a
Adrian Jones <adrianbjones@gmail.com>
2026-09-25 16:59:44 -0700

    SQLite: FTS5 fulltext in databases created with it (marker pw_fulltext_v1), and live tests

--- wire/core/WireDatabase/WireDatabaseDialectSQLite.php
+++ wire/core/WireDatabase/WireDatabaseDialectSQLite.php
@@ -62,6 +62,14 @@ class WireDatabaseDialectSQLite extends WireDatabaseDialect {
 	protected $supportsJson = null;
 
 	/**
+	 * FTS5 fulltext in use for this database? (null until connected, see initConnection())
+	 *
+	 * @var bool|null
+	 *
+	 */
+	protected $fulltext = null;
+
+	/**
 	 * Get dialect name
 	 *
 	 * @return string
@@ -84,6 +92,7 @@ class WireDatabaseDialectSQLite extends WireDatabaseDialect {
 				function() use($database) { return $database->pdo(); },
 				pathinfo(self::databaseFile($this->wire()->config), PATHINFO_FILENAME)
 			);
+			$this->translator->setFulltext($this->fulltext === true);
 		}
 		return $this->translator;
 	}
@@ -341,6 +350,8 @@ class WireDatabaseDialectSQLite extends WireDatabaseDialect {
 		$pdo->exec('PRAGMA busy_timeout=5000');
 		$pdo->exec('PRAGMA foreign_keys=OFF');
 		WireDatabaseSQLiteTranslator::registerFunctions($pdo, self::databaseFile($this->wire()->config));
+		$this->fulltext = $this->setting('fulltext', true) !== false && WireDatabaseSQLiteTranslator::setupFulltext($pdo);
+		if($this->translator !== null) $this->translator->setFulltext($this->fulltext);
 	}
 
 	/**
@@ -617,21 +628,26 @@ class WireDatabaseDialectSQLite extends WireDatabaseDialect {
 	}
 
 	/**
+	 * Supports FULLTEXT and MATCH ... AGAINST? Yes, with FTS5, in a database created with it (see WireDatabaseSQLiteTranslator::setupFulltext())
+	 *
+	 * Disable with $config->dbOptions['sqlite']['fulltext'] = false (before the database is created).
+	 *
 	 * @return bool
 	 *
 	 */
 	public function supportsFulltext() {
-		return false;
+		if($this->fulltext === null) $this->database->pdo(); // connects, see initConnection()
+		return $this->fulltext === true;
 	}
 
 	/**
-	 * Stopwords for the LIKE-based fulltext operators: MySQL's built-in list (as for MyISAM), whatever dbEngine is set
+	 * Stopwords: none when FTS5 is used (every word is indexed), otherwise MySQL's built-in list (as for MyISAM) for the LIKE-based operators
 	 *
 	 * @return array
 	 *
 	 */
 	public function fulltextStopwords() {
-		return DatabaseStopwords::getAll();
+		return $this->supportsFulltext() ? array() : DatabaseStopwords::getAll();
 	}
 
 	/**
